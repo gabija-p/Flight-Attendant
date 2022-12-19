8a762ddb8bc15eb25c8ae1833b2070efc9ef8d17
John Doe <jd@gmail.com>
2022-12-19 21:41:47 +0200

    Extended JWT token's time of validity

--- FlightAttendant/Auth/JwtTokenService.cs
+++ FlightAttendant/Auth/JwtTokenService.cs
@@ -38,7 +38,7 @@ public class JwtTokenService : IJwtTokenService
         (
             issuer: _issuer,
             audience: _audience,
-            expires: DateTime.UtcNow.AddMinutes(5),
+            expires: DateTime.UtcNow.AddMinutes(60),
             claims: authClaims,
             signingCredentials: new SigningCredentials(_authSigningKey, SecurityAlgorithms.HmacSha256)
         );
